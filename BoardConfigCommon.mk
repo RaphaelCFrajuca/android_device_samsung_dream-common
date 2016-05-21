d227afd7b2014d96fbafc5dd9b85380b2f5f7b9e
Fevax <fer.vonarx@gmail.com>
2016-05-21 03:05:50 -0300

    updates for source tree

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -11,9 +11,27 @@ TARGET_SOC := exynos8895
 TARGET_BOOTLOADER_BOARD_NAME := universal8895
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
+TARGET_OTA_ASSERT_DEVICE := dream2lte,dream2ltexx
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Binder
 TARGET_USES_64_BIT_BINDER := true
+
+# Renderscript
+BOARD_OVERRIDE_RS_CPU_VARIANT_32 := cortex-a53
+BOARD_OVERRIDE_RS_CPU_VARIANT_64 := cortex-a57
 
 # CPUsets
 ENABLE_CPUSETS := true
@@ -21,17 +39,35 @@ ENABLE_CPUSETS := true
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
 
-# Image
-BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/exynos8895
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+#BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+
+# Partitions
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
 
 # Packaging
 BLOCK_BASED_OTA := false
 
 # Properties
 TARGET_SYSTEM_PROP := device/samsung/dream-common/system.prop
+
+# Radio
+BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# NFC
+BOARD_NFC_HAL_SUFFIX := universal8895
 
 # SELinux
 BOARD_SEPOLICY_DIRS := device/samsung/dream-common/sepolicy
